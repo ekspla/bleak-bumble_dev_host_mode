bd47cbb96898f589be3091a44b8c587ebdc68fc7
ekspla <ekspla@sky.plala.or.jp>
2026-04-24 10:18:30 +0900

    Change to LE Extended Advertising.

--- examples/client_change_connection_phy.py
+++ examples/client_change_connection_phy.py
@@ -1,4 +1,4 @@
-"""This example demonstrates how to change connection PHY.
+"""This example demonstrates how to get/set connection PHYs.
 
 It was tested with a pair of RTL8761B usb dongles on Linux, 
 and was confirmed to work by using nRF BLE sniffer.
@@ -8,7 +8,7 @@ import asyncio
 
 from bumble import data_types
 from bumble.core import AdvertisingData
-from bumble.device import Device
+from bumble.device import Device, AdvertisingParameters
 from bumble.hci import Address, HCI_LE_1M_PHY, HCI_LE_2M_PHY, HCI_LE_CODED_PHY
 
 from bleak import BleakClient
@@ -17,6 +17,8 @@ from bleak_bumble import start_transport
 from bleak_bumble.client import BleakClientBumble
 from bleak_bumble.scanner import BleakScannerBumble
 
+# True: LE extendend advertising ('1m', 'coded') / False: LE legacy advertising ('1m').
+LE_EXTENDED_ADV = True
 
 async def main():
     transport = await start_transport("usb:0", True)
@@ -27,7 +29,6 @@ async def main():
         hci_source=transport.source,
         hci_sink=transport.sink,
     )
-    bumble_peripheral.advertising_interval_min = bumble_peripheral.advertising_interval_max = 200
     adv_data = [
         data_types.Flags(
             AdvertisingData.Flags.LE_GENERAL_DISCOVERABLE_MODE
@@ -35,10 +36,25 @@ async def main():
         ),
         data_types.CompleteLocalName(bumble_peripheral.name),
     ]
-    bumble_peripheral.advertising_data = bytes(AdvertisingData(adv_data))
     await bumble_peripheral.power_on()
-    await bumble_peripheral.start_advertising()
     
+    if not LE_EXTENDED_ADV:
+        bumble_peripheral.advertising_interval_min = bumble_peripheral.advertising_interval_max = 200
+        bumble_peripheral.advertising_data = bytes(AdvertisingData(adv_data))
+        await bumble_peripheral.start_advertising()
+
+    else:
+        advertising_set = await bumble_peripheral.create_advertising_set(
+            advertising_parameters = AdvertisingParameters(
+                primary_advertising_interval_min = 200,
+                primary_advertising_interval_max = 200,
+                primary_advertising_phy = HCI_LE_1M_PHY,
+                secondary_advertising_phy = HCI_LE_CODED_PHY,
+                secondary_advertising_phy_options = 0,
+            ),
+            advertising_data = bytes(AdvertisingData(adv_data)),
+        )
+
     #await asyncio.sleep(10)
 
     device = await BleakScanner.find_device_by_name(
@@ -61,6 +77,7 @@ async def main():
             #print(f'preferences: {backend._connection_parameters_preferences}')
             phy1 = await backend._connection.get_phy()
             print(phy1)
+
             await backend._connection.set_phy(
                 tx_phys=[HCI_LE_CODED_PHY],
                 rx_phys=[HCI_LE_CODED_PHY],
@@ -69,6 +86,7 @@ async def main():
             await asyncio.sleep(1)
             phy2 = await backend._connection.get_phy()
             print(phy2)
+
             await backend._connection.set_phy(
                 tx_phys=[HCI_LE_2M_PHY],
                 rx_phys=[HCI_LE_2M_PHY],
@@ -76,6 +94,7 @@ async def main():
             await asyncio.sleep(1)
             phy3 = await backend._connection.get_phy()
             print(phy3)
+
             await backend._connection.set_phy(
                 tx_phys=[HCI_LE_1M_PHY],
                 rx_phys=[HCI_LE_1M_PHY],
