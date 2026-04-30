ef42cfc247ba67bbc0b140da5af72a2a8db33374
ekspla <ekspla@sky.plala.or.jp>
2026-04-30 11:51:43 +0900

    Create test_connection_host_mode.py
    ================================ tests coverage ================================
    _______________ coverage: platform linux, python 3.12.6-final-0 ________________
    
    Name                       Stmts   Miss  Cover   Missing
    --------------------------------------------------------
    bleak_bumble/__init__.py      64      6    91%   78-81, 103-104
    bleak_bumble/client.py       198     23    88%   39-40, 127, 142, 161-166, 203-206, 211-214, 232, 249, 274, 328, 352, 374, 393, 424
    bleak_bumble/scanner.py       93      4    96%   31, 189, 209, 228
    bleak_bumble/utils.py          6      0   100%
    --------------------------------------------------------
    TOTAL                        361     33    91%
    =================== 21 passed, 4 skipped in 69.40s (0:01:09) ===================

---
+++ tests/test_connection_host_mode.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""Test for `bleak.backends.bumble` package, specifically connection with `host_mode`.
+
+Although not of great importance, this test checks the `if host_mode==True:` branch.
+"""
+import asyncio
+import pytest
+
+from bumble.controller import Controller
+from bumble.transport.common import Transport
+
+from bleak import BleakClient
+from bleak import BleakScanner
+
+from bleak_bumble import transports, get_link
+from bleak_bumble.client import BleakClientBumble
+from bleak_bumble.scanner import BleakScannerBumble
+
+from tests.test_utils import get_device
+
+CONN_ADDR = "12:34:56:78:AB:CD"
+
+@pytest.mark.asyncio
+async def test_connect_host_mode():
+    async def add_pytest_host_transport():
+        """Connect the host to a virtual controller with LocalLink()."""
+        if "pytest" in transports.keys():
+            return transports["pytest"]
+        controller = Controller(
+            "client",
+            link=get_link(),
+        )
+        transports["pytest"] = Transport(controller, controller)
+        return transports["pytest"]
+
+    conn_dev = get_device(CONN_ADDR)
+    await conn_dev.power_on()
+    await conn_dev.start_advertising()
+
+    _ = await add_pytest_host_transport()
+
+    device = await BleakScanner.find_device_by_name(
+        conn_dev.name,
+        backend=BleakScannerBumble,
+        cfg="pytest",
+        host_mode=True,
+    )
+    assert device is not None
+    assert (transports.pop("pytest", None)) is None
+
+    _ = await add_pytest_host_transport()
+
+    client = BleakClient(
+        CONN_ADDR,
+        backend=BleakClientBumble,
+        cfg="pytest",
+        host_mode=True,
+    )
+
+    await client.connect()
+    await asyncio.sleep(1)
+    assert client.is_connected
+
+    await client.disconnect()
+    assert (transports.pop("pytest", None)) is None
