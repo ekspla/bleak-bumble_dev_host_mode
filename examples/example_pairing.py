076332dc9f85f0ccc64500825dd10ea1117b1c7c
ekspla <ekspla@sky.plala.or.jp>
2026-05-19 13:17:52 +0900

    Add: bond/unpair in pairing example with delegate
    This example was tested to work against a peripheral running `bumble/apps/pair.py` with various combinations of initiator's and responder's I/O capabilities.

--- examples/example_pairing.py
+++ examples/example_pairing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""An example of how to pair with a peripheral using various I/O capabilties:
+"""An example of how to pair/bond with a peripheral using various I/O capabilties:
 'keyboard', 'display', 'display+keyboard', 'display+yes/no' and 'none'.
 
 `Delegate` in `bumble/apps/pair.py` is used for ease of demonstration. 
@@ -13,13 +13,15 @@ import click
 from functools import wraps
 
 from bumble.apps.pair import Delegate  # Use Delegate in apps for ease of demonstrations. 
-from bumble.hci import Address
+from bumble.device import DeviceConfiguration
 from bumble.pairing import PairingConfig
 
 from bleak import BleakClient, BleakScanner
 from bleak_bumble.client import BleakClientBumble
 from bleak_bumble.scanner import BleakScannerBumble
 
+
+CLIENT_BD_ADDR = "F0:F1:F2:F3:F4:F5"
 
 # Reading this characteristics requires pairing.
 HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb"
@@ -50,13 +52,22 @@ def async_cmd(func):
 @click.argument('hci_transport')
 @async_cmd
 async def main(io, hci_transport):
+    dev_cfg = DeviceConfiguration.from_dict(
+        {
+            "name": "client",
+            "address": CLIENT_BD_ADDR,
+            "keystore": "JsonKeyStore",
+            "irk": "865F81FF5A8B486EAAE29A27AD9F77DC",
+        }
+    )
+
     target_device = await BleakScanner.find_device_by_name(
-        "Bumble", backend=BleakScannerBumble, cfg=hci_transport, host_mode=True
+        "Bumble", backend=BleakScannerBumble, cfg=hci_transport, host_mode=True, dev_cfg=dev_cfg
     )
 
     print("Connecting...")
     async with BleakClient(
-        target_device, backend=BleakClientBumble, cfg=hci_transport, host_mode=True
+        target_device, backend=BleakClientBumble, cfg=hci_transport, host_mode=True, dev_cfg=dev_cfg
     ) as client:
 
         print('Connected.')
@@ -68,13 +79,14 @@ async def main(io, hci_transport):
             val = await client.read_gatt_char(hrm_char)
             print(f"Heart rate: {val}")
         except:
-            print("Failed to read.")
-    print("Disconnected")
+            print("Failed to read without pairing.")
+    print("Disconnected.\n")
 
     await asyncio.sleep(2)
 
+    print("Connecting...")
     async with BleakClient(
-        target_device, backend=BleakClientBumble, cfg=hci_transport, host_mode=True
+        target_device, backend=BleakClientBumble, cfg=hci_transport, host_mode=True, dev_cfg=dev_cfg
     ) as client:
 
         print('Connected.')
@@ -101,7 +113,78 @@ async def main(io, hci_transport):
             print(f"Heart rate: {val}")
         except:
             print("Failed to read.")
-        await client.disconnect()
+    print("Disconnected.\n")
+
+    await asyncio.sleep(2)
+
+    print("Connecting...")
+    async with BleakClient(
+        target_device, backend=BleakClientBumble, cfg=hci_transport, host_mode=True, dev_cfg=dev_cfg
+    ) as client:
+
+        print('Connected.')
+        await asyncio.sleep(1)
+
+        # Use Delegate in bumble/apps/pair.py
+        backend = client._backend
+        backend._dev.pairing_config_factory = lambda connection: PairingConfig(
+            sc=SC,
+            mitm=MITM,
+            bonding=BONDING,
+            #oob=oob_contexts,
+            #identity_address_type=identity_address_type,
+            delegate=Delegate(MODE, connection, io, PROMPT),
+        )
+
+        print("Encrypt using the key in keystore...")
+        await client._backend._connection.encrypt()
+
+        # This should succeed.
+        try:
+            hrm_char = client.services.get_characteristic(HEART_RATE_MEASUREMENT)
+            val = await client.read_gatt_char(hrm_char)
+            print(f"Heart rate: {val}")
+        except:
+            print("Failed to read.")
+
+        print("Unpairing...")
+        await client.unpair()
+        key = await client._backend._dev.keystore.get(target_device.address)
+        assert key is None
+        print("Unpaired: the key was deleted.")
+    print("Disconnected.\n")
+
+    await asyncio.sleep(2)
+
+    print("Connecting...")
+    async with BleakClient(
+        target_device, backend=BleakClientBumble, cfg=hci_transport, host_mode=True, dev_cfg=dev_cfg
+    ) as client:
+
+        print('Connected.')
+        await asyncio.sleep(1)
+
+        # Use Delegate in bumble/apps/pair.py
+        backend = client._backend
+        backend._dev.pairing_config_factory = lambda connection: PairingConfig(
+            sc=SC,
+            mitm=MITM,
+            bonding=BONDING,
+            #oob=oob_contexts,
+            #identity_address_type=identity_address_type,
+            delegate=Delegate(MODE, connection, io, PROMPT),
+        )
+
+        key = await client._backend._dev.keystore.get(target_device.address)
+        assert key is None
+        print("The key does not exist.")
+
+        # This should fail.
+        print("Try to encrypt without the key in keystore...")
+        try:
+            await client._backend._connection.encrypt()
+        except:
+            print("Failed to encrypt without the key.")
     print("Disconnected")
 
 
